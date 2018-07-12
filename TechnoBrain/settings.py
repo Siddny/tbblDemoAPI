373b68853e13ebdb0ada36f080bccc4594c950fc
siddny <cdohzangah@gmail.com>
2018-07-12 22:54:25 +0300

    debug = false

--- TechnoBrain/settings.py
+++ TechnoBrain/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+@p#r5n+*vncdb-!cvq$$n7(ms^vn&5dbr-!aye*kr7j^$s3&u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*',
