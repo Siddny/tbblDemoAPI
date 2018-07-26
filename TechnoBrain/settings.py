48885100e77c475ab09f6f3f25b7ffacf20c119b
siddny <cdohzangah@gmail.com>
2018-07-27 00:39:54 +0300

    SMS API intact

--- TechnoBrain/settings.py
+++ TechnoBrain/settings.py
@@ -62,10 +62,10 @@ MIDDLEWARE = [
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sidneykipronoh@gmail.com'
+EMAIL_HOST_USER = 'emailaddressflani@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = '1kazanga1'
+EMAIL_HOST_PASSWORD = 'passwordyako'
 
 ROOT_URLCONF = 'TechnoBrain.urls'
 
